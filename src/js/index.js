a345fbf1e9d6fbfb0828f556353581c04356f56b
Nick Fletcher <nickeax@gmail.com>
2020-07-20 13:02:20 +1000

    Fixed truncation issue

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,13 @@
-const MAX_LEN = 6;
+const MAX_FRETS = 9
+const MIN_FRETS = 6
+
 const inp = document.querySelector('#inp')
 const output = document.querySelector('#output')
 const list = document.querySelector('#list')
+const clearAll = document.querySelector('#clearAll')
+
+clearAll.classList.add('buttons')
+
 let allChordsArr = []
 let allFretBoardsArr = []
 const frets = [
@@ -28,7 +34,7 @@ class FretPosition {
         CL.push(t === 'note' ? 'segment--note' : 'segment--empty')
         CL.push(isFirst ? 'nut' : 'segment--empty')
         let tmp = newElement('span', CL)
-        
+
         let DCL = []
         DCL.push(t === 'note' ? 'dot' : 'dot--hidden')
         DCL.push(isFirst ? 'nutNote' : 'dot-hidden')
@@ -54,20 +60,36 @@ class FretBoard {
     stringNumber
     inputFrets = []
 
-    constructor(str, fn = 7, sn = 6) {
+    constructor(str, fn = MIN_FRETS, sn = 6) {
         this.strings = []
         this.fretNumber = fn
         this.stringNumber = sn
         this.base = document.createElement('div')
         this.base.classList.add('fretboard', 'base')
         let tmpArr = str.split('')//.reverse()
-        this.inputFrets = tmpArr.map(x => {
+        
+        tmpArr = tmpArr.map(x => {
             return parseInt(x)
         })
 
+        this.fretNumber = tmpArr.reduce((a, c) => {
+            a = Math.max(a, c)
+            return a 
+        }, 0)
+
+        if(this.fretNumber > MAX_FRETS) {
+            this.fretNumber = MAX_FRETS
+        }
+
+        if(this.fretNumber <= MIN_FRETS) {
+            this.fretNumber = MIN_FRETS
+        }
+        
+        this.inputFrets = tmpArr
+
         for (let i = this.stringNumber - 1; i >= 0; i--) {
             let tmpSeg = []
-            for (let y = 0; y < this.fretNumber; y++) {
+            for (let y = 0; y <= this.fretNumber; y++) {
                 let flag = false;
                 if (this.inputFrets[i] === y) {
                     tmpSeg.push(new FretPosition('note', translateFretNumber(y, i), y == 0, i))
@@ -90,13 +112,28 @@ class FretBoard {
 }
 
 inp.addEventListener('keyup', (eve) => {
-    
+
     inp.value = isALlowed(inp.value)
+    inp.value = stripSpaces(inp.value)
     // if(eve.keyCode < 48 || eve.keyCode >= 57) {
     //     inp.value = ""
     //     return
     // }
     displayOutput(inp.value)
+})
+
+clearAll.disabled = true
+
+clearAll.addEventListener("click", ev => {
+    if (confirm("Are you sure? All your chords will be lost...")) {
+        allChordsArr = []
+        allFretBoardsArr = []
+        output.innerHTML = ""
+    }
+
+    clearAll.disabled = true
+
+    showAll()
 })
 
 inp.focus()
@@ -110,22 +147,33 @@ function isALlowed(chr) {
     return tmpArr.join('')
 }
 
+function stripSpaces(str) {
+    let tmp = str.split('')
+
+    tmp = tmp.filter(x => {
+        return x != ' '
+    })
+
+    return tmp.join('')
+}
+
 function newElement(type, cl) {
     let tmp = document.createElement(type)
     tmp.classList.add(...cl)
     return tmp
 }
+
 function displayOutput(str) {
-    if (str.length > MAX_LEN) str = str.slice(0, MAX_LEN)
+    if (str.length > MAX_FRETS) str = str.slice(0, MAX_FRETS)
     let tmpArr = str.split('')
     let tmpArr2 = []
     let tmpStr = ""
-    let builder = ""
-    let tmpBtn = document.createElement('button')
-    if(inp.value.length > 0) {
+    let tmpBtn = document.createElement('span')
+    tmpBtn.classList.add('buttons', 'btnSave')
+    if (inp.value.length > 0) {
         tmpBtn.innerText = "SAVE"
     } else {
-        tmpBtn.innerHTML = ""
+        tmpBtn.innerText = ""
     }
 
     tmpArr.forEach((x, i) => {
@@ -133,13 +181,12 @@ function displayOutput(str) {
     })
 
     output.innerHTML = ""
+    output.innerHTML = "<h3>Current notes <tt>(to add, click SAVE)</tt></h3>"
     tmpArr2.forEach((x, i) => {
         tmpStr += `${x} `
         output.innerHTML += `<span class="noteCell">${x}</span>`
     })
 
-    output.innerHTML += builder
-    builder += `<span>${tmpStr}</span>`
     output.appendChild(tmpBtn)
     tmpBtn.addEventListener('click', ev => {
         allFretBoardsArr.push(new FretBoard(inp.value))
@@ -171,8 +218,13 @@ function removeAllChildNodes(parent) {
 
 function showAll() {
     removeAllChildNodes(list)
-
+    
+    if (allChordsArr.length > 0) {
+        clearAll.disabled = false
+    } 
+    
     allFretBoardsArr.forEach((x, i) => {
+        console.log(`INDEX: ${i}`);
         let fbAnchor = newElement('div', ['listRow'])
         fbAnchor.innerHTML = drawNotesRow(i)
         x.draw(fbAnchor)
@@ -185,7 +237,7 @@ function drawNotesRow(row) {
     let tmp = ""
     tmp = `<div class="notesRow">`
     allChordsArr[row].forEach((x, i) => {
-        if (i + 1 % MAX_LEN === 0) tmp += `<br />`
+        if (i + 1 % MAX_FRETS === 0) tmp += `<br />`
         tmp += `<div class="listCellParent"><span class="listCell  ${selectAnim()}">${x}</span></div>`
     })
     tmp += `</div>`
